ecfca42ab0671b5122dec7d848a9703818e934c4
harshmalviya7 <52200329+harshmalviya7@users.noreply.github.com>
2020-08-30 20:22:34 +0530

    Update first.py

--- Sample Project/first.py
+++ Sample Project/first.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
 
-PATH='C:/Users/aarti computer/Desktop/oneday/Selenium/chromedriver.exe'
+PATH='C:/Users/Desktop/Selenium/chromedriver.exe'
 
-driver=webdriver.Chrome(PATH)
+driver=webdriver.Chrome(executable_path=PATH)
 
 driver.get("https://en.wikipedia.org/wiki/Wiki")
+#opens the wikipedia
 
 driver.close()
+#closes the chrome
 
 driver.quit()
+#quits the driver
 
 print("Test complete")
+#output generated in cmd prompt
